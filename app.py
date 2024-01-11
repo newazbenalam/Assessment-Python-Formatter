3a62584b3c7de0426440a23da35fbb1a99f9162b
newazbenalam <newazbenalam@gmail.com>
2024-01-11 17:09:56 +0600

    main app

---
+++ app.py
@@ -0,0 +1,16 @@
+import argparse
+import os
+
+from app import formatJsonDoc
+
+if __name__ == '__main__':
+  parser = argparse.ArgumentParser(description='Python formatter')
+  parser.add_argument('--input', dest='input', type=str, help='Name of the input file')
+  args = parser.parse_args()
+  
+  out = formatJsonDoc(args.input)
+
+  with open("formatted_"+os.path.basename(args.input), "w") as outfile:
+    outfile.write(out)
+
+  print(args.input)
