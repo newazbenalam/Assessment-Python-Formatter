4afeee270ffd56c9a52c0f64d1e4bdde9b3b10fe
newazbenalam <newazbenalam@gmail.com>
2024-01-11 17:10:32 +0600

    None value dict

---
+++ app/dictionary.py
@@ -0,0 +1,29 @@
+def sampleDict():
+    return dict
+
+
+annotation_objects_dict = {
+    "vehicle": {
+        "presence": 0,
+        "bbox": []
+    },
+    "license_plate": {
+        "presence": 0,
+        "bbox": []
+    }
+}
+
+annotation_attributes_dict = {
+            "vehicle": {
+                "Type": None,
+                "Pose": None,
+                "Model": None,
+                "Make": None,
+                "Color": None
+            },
+            "license_plate": {
+                "Difficulty Score": None,
+                "Value": None,
+                "Occlusion": None
+            }
+        }
